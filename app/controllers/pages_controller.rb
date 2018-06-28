1d80c2f4250fea40e58bfa2747ae85bc7d9813bb
Ignacio Contreras Pinilla <ignacio.con.pin@gmail.com>
2018-06-28 08:52:32 +0200

    Add author views and individual page view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,11 @@ class PagesController < ApplicationController
     render :index
   end
 
+  def show
+    @page = Page.find(params[:id])
+    render :show
+  end
+
   protected
 
   def last_pages_as_json
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pages, only: [:index] do
+  resources :pages, only: [:index, :show] do
     # Nested routes and resources
     # get :authors, to: "authors#index"
   end
+
+  resources :authors, only: [:show]
 end
